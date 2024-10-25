465c7e6bd353e67a8d50061550ea4bbb9fea9702
proxyau <stuart.lillywhite@gmail.com>
2024-10-26 01:59:37 +1100

    updates

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from constants import *
 
 def main():
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers(updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
@@ -15,7 +18,8 @@ def main():
                 return
         
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        drawable.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
